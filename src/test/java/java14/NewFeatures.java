5f58f7761a4a66205ec90731f07c31109c5c5fb9
gannons <gannons@cisco.com>
2022-07-02 11:58:50 +0100

    Java 14

---
+++ src/test/java/java14/NewFeatures.java
@@ -0,0 +1,56 @@
+package java14;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * https://www.baeldung.com/java-13-new-features
+ */
+public class NewFeatures {
+    @ParameterizedTest(name = "{arguments}")
+    @ValueSource(strings = {"MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"})
+    public void releasedSwitch(String day) {
+        boolean isTodayHoliday;
+        switch (day) {
+            case "MONDAY":
+            case "TUESDAY":
+            case "WEDNESDAY":
+            case "THURSDAY":
+            case "FRIDAY":
+                isTodayHoliday = false;
+                break;
+            case "SATURDAY":
+            case "SUNDAY":
+                isTodayHoliday = true;
+                break;
+            default:
+                throw new IllegalArgumentException("What's a " + day);
+        }
+
+        // versus
+
+        isTodayHoliday = switch (day) {
+            case "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY" -> false;
+            case "SATURDAY", "SUNDAY" -> true;
+            default -> throw new IllegalArgumentException("What's a " + day);
+        };
+
+    }
+
+    @Test
+    public void textBlock() {
+        String TEXT_BLOCK_JSON = """
+{
+    "name" : "Baeldung",
+    "website" : "https://www.%s.com/"
+}""";
+        assertTrue(TEXT_BLOCK_JSON.contains("Baeldung"));
+        assertTrue(TEXT_BLOCK_JSON.indexOf("www") > 0);
+        assertTrue(TEXT_BLOCK_JSON.length() > 0);
+
+        System.out.println(TEXT_BLOCK_JSON);
+    }
+}
